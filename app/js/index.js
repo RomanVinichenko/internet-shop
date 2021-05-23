bbe23ca12be12770c7c48166cbe73a3e23fa71bf
RomanVinichenko <chicagezzz@gmail.com>
2021-05-23 23:54:01 +0300

    fix slider

--- app/js/index.js
+++ app/js/index.js
@@ -2,13 +2,21 @@ $(function () {
     $('.intro-top').slick({
         dots: true,
         arrows: false,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        pauseOnFocus: false,
+        pauseOnHover: false,
     });
 
-    $('.partners-items').slick({
+    $('.partners__items').slick({
         dots: false,
         arrows: false,
         slidesToShow: 5,
         slidesToScroll: 3,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        pauseOnFocus: false,
+        pauseOnHover: false,
     });
 
     var mixer = mixitup('.products');
